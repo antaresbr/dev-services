34767bad221f534d2fb36e2a9dfcc0a4588bc1a9
Eduardo Guerra <eduardo.antares@gmail.com>
2024-06-24 12:13:11 -0300

    Add nginx/nginx-restart.sh

---
+++ data/nginx/nginx-restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SAIL_DIR="$(dirname "$(realpath "$0")")/sail"
+while [ ! -d "${SAIL_DIR}" ]
+do
+  [ "${SAIL_DIR}" != "/" ] || { echo "$(basename "$0") | ERROiR: Impossible to get SAIL_DIR"; exit 1; }
+  [ ! -d "${SAIL_DIR}" ] || break
+  SAIL_DIR="$(dirname "$(dirname "${SAIL_DIR}")")/sail"
+done
+
+"${SAIL_DIR}/sail" exec nginx "nginx -t && nginx -s reload"
+
