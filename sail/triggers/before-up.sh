834f009dd09cab51b7434973f3c09479d157fdf7
Eduardo Guerra <eduardo.antares@gmail.com>
2026-03-10 21:54:03 -0300

    Create sail/triggers/before-up.sh; sail/.example/data/myadmin/config.user.inc.php and phpmyadmin-local.ini

---
+++ sail/triggers/before-up.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+[ -d "${SAIL_DIR}" ] || { echo "$(basename "$0") | SAIL_DIR not found: ${SAIL_DIR}"; exit 1; }
+
+if [ ${SAIL_MYADMIN,,} == "true" ]
+then
+  targetDir="$(dirname "${SAIL_DIR}")/data/myadmin"
+  [ -d "${targetDir}" ] || mkdir -p "${targetDir}"
+
+  targetFile="${targetDir}/config.user.inc.php"
+  if [ ! -f "${targetFile}" ]
+  then
+    sourceFile="${SAIL_DIR}/.example/data/myadmin/$(basename "${targetFile}")"
+    if [ -f "${sourceFile}" ]
+    then
+      cp "${sourceFile}" "${targetFile}"
+    else
+      echo "<?php" > "${targetFile}"
+      echo "/* local config */" >> "${targetFile}"
+    fi
+  fi
+
+  targetFile="${targetDir}/phpmyadmin-local.ini"
+  if [ ! -f "${targetFile}" ]
+  then
+    sourceFile="${SAIL_DIR}/.example/data/myadmin/$(basename "${targetFile}")"
+    if [ -f "${sourceFile}" ]
+    then
+      cp "${sourceFile}" "${targetFile}"
+    else
+      echo "# local config" >> "${targetFile}"
+    fi
+  fi
+fi
